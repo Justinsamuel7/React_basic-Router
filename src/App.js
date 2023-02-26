f65de12b92e571e518bf6370e363fe54710b84e9
Justinsamuel7 <119278858+Justinsamuel7@users.noreply.github.com>
2023-02-26 20:45:10 +0530

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./style.css";
 import { Routes, Route, Link, useNavigate } from 'react-router-dom';
-
+import Home from "./Home"
+import Career from "./Career"
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Career" element={<Career/>}/>
+
+      </Routes>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello This is home page!</h1>
+      <Link to="/Career">Career</Link>
+    </div>
+  );
+}
